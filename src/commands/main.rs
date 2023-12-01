97decf8bf6225749f9d021386be436ba6a35d8f8
David Thomas <david2005thomas@gmail.com>
2023-12-01 10:14:17 +0000

    Fix licence comments

--- src/commands/main.rs
+++ src/commands/main.rs
@@ -1,11 +1,11 @@
 // Discord TTS Bot
 // Copyright (C) 2021-Present David Thomas
-
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
 // by the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -13,6 +13,7 @@
 
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use std::{borrow::Cow, num::NonZeroU16};
 
 use songbird::error::JoinError;
